3be51e66192db23648a8c0cecdce001d0e3c9095
Wesley Shee <wesleys@mit.edu>
2026-03-04 00:22:47 -0500

    Initial commit

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Contact</h1>
+
+<form action="mailto:wesleys@mit.edu" method="GET">
+  <label>
+    Your email:
+    <input name="email" type="email" />
+  </label>
+
+  <label>
+    Subject:
+    <input name="subject" />
+  </label>
+
+  <label>
+    Message:
+    <textarea name="body"></textarea>
+  </label>
+
+  <button>Send</button>
+</form>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+</script>
+
+<section class="projects">
+  <h1>{projects.length} Projects</h1>
+
+  {#each projects as p}
+    <Project data={p} />
+  {/each}
+</section>
